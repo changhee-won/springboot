f7230183fe9f2b5ce4139d99d1af7b7787cf0857
changhee.won <mikywon@gmail.com>
2024-12-07 12:55:33 +0900

    update Datacontroller

--- demo/src/main/java/com/example/demo/repository/MyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.MyDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MyRepository extends MongoRepository<MyDocument, String> {
-}
